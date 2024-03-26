2eea62c8ecd0a21a5ac334dbc6e93963f38e0566
hsoulsboy <hsoulsboy@mail.com>
2024-03-25 23:33:48 -0300

    Add brief descriptions to the DB functions

--- db/functions.go
+++ db/functions.go
@@ -13,6 +13,8 @@ const (
 	uri    = "mongodb://root:root@localhost:27017/"
 )
 
+// GetConnection opens a connection with a target Mongo database
+// It returns a Mongo client for further operations
 func GetConnection() (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI(uri)
 
@@ -30,12 +32,16 @@ func GetConnection() (*mongo.Client, error) {
 	return client, nil
 }
 
+// GetCollection retrieves or creates a collection from a database
+// It returns a Mongo collection for further operations
 func GetCollection(connection *mongo.Client, collectionName string) *mongo.Collection {
 	collection := connection.Database(dbName).Collection(collectionName)
 
 	return collection
 }
 
+// CountAllDocuments counts the number of documents present in a Mongo collection
+// It returns an integer counter of documents
 func CountAllDocuments(collection *mongo.Collection) (int64, error) {
 	numOfDocs, err := collection.CountDocuments(context.Background(), bson.M{})
 	if err != nil {
